3ab45b4b6b64018a0862c63b65d83373a27217e7
LeonidGHA <104501564+LeonidGHA@users.noreply.github.com>
2022-12-10 14:21:25 +0300

    fix send grid email

--- helpers/sendGridMail.js
+++ helpers/sendGridMail.js
@@ -1,11 +1,12 @@
 const sgMail = require("@sendgrid/mail");
 
 const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_EMAIL } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendGridMail = async (data) => {
-  const mail = { ...data, from: "leonidsestrivatovskiy@gmail.com" };
+  const mail = { ...data, from: `${SENDGRID_EMAIL}@gmail.com` };
   await sgMail.send(mail);
   return true;
 };
